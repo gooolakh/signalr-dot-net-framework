904af500214c6cdfa88e795f231d2662a941c95b
Majid Fathi <majidfathi69@gmail.com>
2021-07-08 12:08:23 +0430

    first commit

---
+++ sslwebapp/ChatHub.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+namespace sslwebapp
+{
+    public class ChatHub : Hub
+    {
+        public override Task OnConnected()
+        {
+            return base.OnConnected();
+        }
+
+        public override Task OnReconnected()
+        {
+            return base.OnReconnected();
+        }
+
+        public void Send(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.broadcastMessage(name, message);
+        }
+    }
+}
---
+++ sslwebapp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+[assembly: OwinStartup(typeof(sslwebapp.Startup))]
+namespace sslwebapp
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+        }
+    }
+}
